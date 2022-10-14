67c6740d7ae83b4dcf7536468313219a39fe7fa1
b.chirani <chirani124@gmail.com>
2022-10-14 19:55:50 +0100

    remove text decoration

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 let sectionList = ["intro", "portfolio", "experience", "education"];
 let lastItem = "intro";
 let debounce = new Date().getTime();
-//console.log(Date().getTime());
+
 function SelectSection(selected) {
   let current = new Date();
-  if (current - debounce < 1000) return;
+  if (current - debounce < 500) return;
   debounce = current;
   let el = document.getElementById(selected);
 
   el.style.zIndex = 210;
-  el.style.animation = "1s slideIn";
+  el.style.animation = "0.5s slideIn";
   let unselected = sectionList.filter((item) => item !== selected);
   setTimeout(
     () =>
       unselected.forEach((item) => {
         document.getElementById(item).style.animation = "0.01s slideOut";
       }),
-    900
+    490
   );
   unselected.forEach((item) => {
     document.getElementById(item).style.zIndex = 100;
   });
   document.getElementById(lastItem).style.zIndex = 109;
   el.style.zIndex = 110;
-  console.log(unselected);
   lastItem = selected;
-  console.log(info);
 }
